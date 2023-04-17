9f6cdcf566223825c545689f2aa018bb2dbd3d47
dmdkhao <1015835653@qq.com>
2023-04-18 00:21:28 +0800

    add 读取客户端内容事件

--- src/test/java/com/ddk/netty/nio/selector/Server.java
+++ src/test/java/com/ddk/netty/nio/selector/Server.java
@@ -1,12 +1,11 @@
 package com.ddk.netty.nio.selector;
 
+import com.ddk.netty.nio.utils.ByteBufferUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -21,6 +20,7 @@ public class Server {
     public static void main(String[] args) {
         try {
             ServerSocketChannel ssc = ServerSocketChannel.open();
+            // selector需要channel工作在非阻塞模式
             ssc.configureBlocking(false);
 
             //1. create selector
@@ -44,10 +44,23 @@ public class Server {
                 while (iterator.hasNext()) {
                     SelectionKey key = iterator.next();
                     log.debug("key={}", key.toString());
-//                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
-//                    SocketChannel socketChannel = serverSocketChannel.accept();
-//                    log.debug("socketChannel={}", socketChannel);
-                    key.cancel();
+                    if (key.isAcceptable()) {
+                        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                        SocketChannel sc = serverSocketChannel.accept();
+                        log.debug("socketChannel={}", sc);
+                        //register client channel to selector
+                        sc.configureBlocking(false);
+                        SelectionKey scSelectionKey = sc.register(selector, 0, null);
+                        scSelectionKey.interestOps(SelectionKey.OP_READ);
+                    } else if (key.isReadable()) {
+                        // client channel trigger read event
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(16);
+                        channel.read(buffer);
+                        buffer.flip();
+                        ByteBufferUtil.debugRead(buffer);
+                    }
+//                    key.cancel();
                 }
             }
 
